2a0cf7942b94c6254dbca660aa5101d7fe476802
kashitakashi <kashi_takashi@twilightseed.com>
2025-02-08 09:36:32 -0600

    Update FALCXBuilder.sh

--- FALCXBuilder.sh
+++ FALCXBuilder.sh
@@ -10,6 +10,9 @@ tar -C /usr/local -xzf go1.23.6.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 mkdir /FreezyArena
+cp ./FreezyArena.service /FreezyArena
 cd /FreezyArena
 git clone -b 2025-Reefscape-Week-0 https://github.com/cpapplefamily/freesy-arena.git /FreezyArena
 go build
+
+systemctl link /FreezArena/FreezyArena.service
