9397d174f90cd546e92520a25d3ff0e0c831d6ac
kashitakashi <kashi_takashi@twilightseed.com>
2025-02-08 10:05:00 -0600

    Update FALCXBuilder.sh

--- FALCXBuilder.sh
+++ FALCXBuilder.sh
@@ -2,7 +2,7 @@
 apt update
 apt dist-upgrade -y
 apt install git -y
-
+git clone https://github.com/kashitakashi/Freezy-Builder.git .
 wget https://go.dev/dl/go1.23.6.linux-amd64.tar.gz
 
 tar -C /usr/local -xzf go1.23.6.linux-amd64.tar.gz
