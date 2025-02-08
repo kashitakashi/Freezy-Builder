660b9510ccb8424b261abeb6dc422d334f733023
kashitakashi <kashi_takashi@twilightseed.com>
2025-02-08 10:34:15 -0600

    Update FALCXBuilder.sh

--- FALCXBuilder.sh
+++ FALCXBuilder.sh
@@ -2,7 +2,6 @@
 apt update
 apt dist-upgrade -y
 apt install git -y
-git clone https://github.com/kashitakashi/Freezy-Builder.git .
 wget https://go.dev/dl/go1.23.6.linux-amd64.tar.gz
 
 tar -C /usr/local -xzf go1.23.6.linux-amd64.tar.gz
@@ -10,10 +9,20 @@ tar -C /usr/local -xzf go1.23.6.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 mkdir /FreezyArena
-
 git clone -b 2025-Reefscape-Week-0 https://github.com/cpapplefamily/freesy-arena.git /FreezyArena
-cp ./FreezyArena.service /FreezyArena
+
 cd /FreezyArena
-go build
+echo "[Unit]
+Description=Freezy Arena Services
+[Service]
+Type=Simple
+RestartSec=1
+ExecStart=/FreezyArena/cheesy-arena-lite
+WorkingDirectory=/FreezyArena/
+[Install]
+WantedBy=multi-user.target
+go build" > FreezyArena.service
 
 systemctl link /FreezyArena/FreezyArena.service
+systemctl enable FreezyArena.service
+systemctl start FreezyArena.service
